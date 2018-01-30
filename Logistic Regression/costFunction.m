5e8aa41448f4965d62ce905f2a81865ad2380efc
Jordan <jordancharest3@gmail.com>
2018-01-30 17:03:08 -0500

    Simplified cost function implementations

--- Logistic Regression/costFunction.m
+++ Logistic Regression/costFunction.m
@@ -7,12 +7,8 @@ function [cost, gradient] = costFunction(theta, X, y)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% Calculate the hypothesis for all training examples
-hypothesis = X .* repmat(theta', [m 1]);
-hypothesis =  sum(hypothesis, 2);
-
 % Generate the logistic distribution
-hypothesis = sigmoid(hypothesis);
+hypothesis = sigmoid(X * theta);
 
 % Calculate cost
 cost = (1/m) * (-y'*log(hypothesis) - (1-y)' * log(1-hypothesis));
@@ -22,21 +18,6 @@ cost = (1/m) * (-y'*log(hypothesis) - (1-y)' * log(1-hypothesis));
 % calculate the cost (by subtracting y)
 % then calculate the partial derivative for each feature in X (just
 % multiply by X)
-delta = repmat((sum(hypothesis, 2) - y), [1 size(X,2)]) .* X;
-
-% Sum the partial derivatives
-delta = (1/m) * sum(delta, 1);
-
-% store the updates in a vector for fminunc() (NOTE: DOES NOT include alpha, as is the case with gradient descent)
-gradient = delta';
-
-
-
-
-
-
-
-
-% =============================================================
+gradient = (1/m) * (X' * (hypothesis - y));
 
 end
--- Logistic Regression/costFunctionReg.m
+++ Logistic Regression/costFunctionReg.m
@@ -8,11 +8,11 @@ function [cost, gradient] = costFunctionReg(theta, X, y, lambda)
 m = length(y); % number of training examples
 
 % Calculate the hypothesis for all training examples
-hypothesis = X .* repmat(theta', [m 1]);
-hypothesis =  sum(hypothesis, 2);
+% hypothesis = X .* repmat(theta', [m 1]);
+% hypothesis =  sum(hypothesis, 2);
 
 % Generate the logistic distribution
-hypothesis = sigmoid(hypothesis);
+hypothesis = sigmoid(X * theta);
 
 % Calculate cost
 cost = (1/m) * (-y'*log(hypothesis) - (1-y)' * log(1-hypothesis));
@@ -25,13 +25,7 @@ cost = cost + lambda/(2*m) * (sum(theta(2:end).^2));
 % calculate the cost (by subtracting y)
 % then calculate the partial derivative for each feature in X (just
 % multiply by X)
-delta = repmat((sum(hypothesis, 2) - y), [1 size(X,2)]) .* X;
-
-% Sum the partial derivatives
-delta = (1/m) * sum(delta, 1);
-
-% store the updates in a vector for fminunc() (NOTE: DOES NOT include alpha, as is the case with gradient descent)
-gradient = delta';
+gradient = (1/m) * (X' * (hypothesis - y));
 
 % store theta_0 because we don't want to regularize it
 temp = gradient(1);
@@ -39,12 +33,5 @@ temp = gradient(1);
 % Add regularization term, remove regularization from theta_0
 gradient = gradient + (lambda/m * theta);
 gradient(1) = temp;
-
-
-
-
-
-
-% =============================================================
 
 end
